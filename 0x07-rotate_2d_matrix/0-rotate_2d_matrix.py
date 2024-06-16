55707e576ff2e8eafa452f3cdf17d8a587997110
ICTECHBAE <iwuohachioma30@gmail.com>
2024-06-16 05:04:30 -0700

    Cleared out empty spaces

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,17 +5,17 @@ Rotate 2D matrix
 
 
 def rotate_2d_matrix(matrix):
-        """
-            Function to rotate 2D matrix
+    """
+    Function to rotate 2D matrix
 
-                Args:
-                        matrix: 2D matrix
-                            """
-                                n = len(matrix)
+    Args:
+        matrix: 2D matrix
+    """
+    n = len(matrix)
 
-                                    for r in range(n):
-                                                for c in range(r, n):
-                                                                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+    for r in range(n):
+        for c in range(r, n):
+            matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
 
-                                                                    for row in matrix:
-                                                                                row.reverse()
+    for row in matrix:
+        row.reverse()
